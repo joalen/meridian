48f601af92a316a66a1df33dd6501aebef3c1843
Alen Jo <jo.alen1@outlook.com>
2026-09-05 23:23:08 +0000

    Refactor driver code to utilize `GenericOptionsParser`
    Now that I'm ready to test out the first part, it is now refactored to make it support the GenericOptionsParser and allow us to run subproblems

--- src/main/java/com/joalen/Q1Analysis.java
+++ src/main/java/com/joalen/Q1Analysis.java
@@ -9,26 +9,46 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 import com.joalen.WordCount.SumReducer;
-import com.joalen.WordCount.TokenizerMapper;
+import com.joalen.WordCount.WordCountMapper;
 
 public class Q1Analysis {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException
     { 
         Configuration config = new Configuration(); 
-        Job job = Job.getInstance(config, "word count");
-        
+        String[] otherArgs = new GenericOptionsParser(config, args).getRemainingArgs();
+
+        if (otherArgs.length != 3) {
+            System.err.println("Usage: Q1Analysis <in> <out> <part>");
+            System.err.println("part: A (WordCount), B (TargetWords), or C (Pattern)");
+            System.exit(2);
+        }
+
+        String in = otherArgs[0];
+        String out = otherArgs[1];
+        String part = otherArgs[2];
+
+        Job job = Job.getInstance(config, "Q1 Analysis - Part " + part);
         job.setJarByClass(Q1Analysis.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(SumReducer.class);
-        job.setReducerClass(SumReducer.class);
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        switch (part)
+        { 
+            case "A": 
+                job.setMapperClass(WordCountMapper.class);
+                job.setCombinerClass(SumReducer.class);
+                job.setReducerClass(SumReducer.class);
+                job.setOutputKeyClass(Text.class);
+                job.setOutputValueClass(IntWritable.class);
+                break;
+            default:
+                System.err.println("Part " + part + " not implemented yet.");
+                System.exit(2);
+        } 
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(in));
+        FileOutputFormat.setOutputPath(job, new Path(out));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
