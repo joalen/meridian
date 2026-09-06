ea7fec8bcf3dab1dde2b7f269583dff88f4af1b5
Alen Jo <jo.alen1@outlook.com>
2026-09-06 05:14:57 +0000

    Move BestWordReducer into Q2Analysis
    Moved BestWordReducer into Q2Analysis file

--- src/main/java/com/joalen/Q2Analysis.java
+++ src/main/java/com/joalen/Q2Analysis.java
@@ -16,7 +16,6 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-import com.joalen.MostFrequentLongWord.BestWordReducer;
 
 public class Q2Analysis {
     /** 
@@ -207,6 +206,44 @@ public class Q2Analysis {
             { 
                 outVal.set(bestWord + "," + bestCount);
                 context.write(key, outVal);
+            }
+        }
+    }
+
+    /**
+     * Final reducer after combiner stage that selects single longest-list word overall
+     * 
+     */
+    static class BestWordReducer extends Reducer<Text, Text, Text, IntWritable> {
+        private final Text outKey = new Text(); 
+        private final IntWritable outVal = new IntWritable(); 
+
+        /** 
+         * Finds the pairing of word to count with the highest count among all values 
+         * for key and emits it as another pair of word to count.
+         */
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{ 
+            String bestWord = null;
+            int bestCount = -1; 
+
+            for (Text value : values)
+            { 
+                String[] parts = value.toString().split(",");
+                String word = parts[0]; 
+
+                int count = Integer.parseInt(parts[1]);
+                if (count > bestCount)
+                { 
+                    bestCount = count; 
+                    bestWord = word;
+                }
+            }
+
+            if (bestWord != null)
+            { 
+                outKey.set(bestWord);
+                outVal.set(bestCount);
+                context.write(outKey, outVal);
             }
         }
     }
