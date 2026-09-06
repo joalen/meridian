d97e6356ec2f059a724a37f255175b50232fcb90
Alen Jo <jo.alen1@outlook.com>
2026-09-06 05:13:22 +0000

    Move BestWordCombiner into Q2Analysis
    Moved contents of BestWordCombiner into Q2Analysis file

--- src/main/java/com/joalen/Q2Analysis.java
+++ src/main/java/com/joalen/Q2Analysis.java
@@ -16,7 +16,6 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-import com.joalen.MostFrequentLongWord.BestWordCombiner;
 import com.joalen.MostFrequentLongWord.BestWordReducer;
 
 public class Q2Analysis {
@@ -165,6 +164,50 @@ public class Q2Analysis {
             }
 
             return listPart.split(",\\s*").length;
+        }
+    }
+
+    /** 
+     * Combiner to help locally reduce pairings of word to count down to a single pair with highest 
+     * count for each key found.
+     */
+    static class BestWordCombiner extends Reducer<Text, Text, Text, Text> {
+        private final Text outVal = new Text(); 
+
+        /** 
+         * Finds the pairings of word to count, where it has highest count among all values for 
+         * key and emits only that one
+         * 
+         * @param key shared constant key from mapper stage
+         * @param values pairings of word to count for comparison 
+         * @param context MapReduce mapper that contains the next payload to emit key to bestWord and bestCount 
+         * 
+         * @throws IOException system halts for I/O issues
+         * @throws InterruptedException reduce() stage interrupted by system  
+         */
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException { 
+            String bestWord = null; 
+            int bestCount = -1; 
+
+            for (Text value : values)
+            { 
+                String[] parts = value.toString().split(",");
+                
+                String word = parts[0]; 
+                int count = Integer.parseInt(parts[1]);
+                
+                if (count > bestCount)
+                { 
+                    bestCount = count; 
+                    bestWord = word;
+                }
+            }
+
+            if (bestWord != null)
+            { 
+                outVal.set(bestWord + "," + bestCount);
+                context.write(key, outVal);
+            }
         }
     }
 
