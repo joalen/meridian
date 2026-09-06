18d6b4feae2777bc09fa3fcafe446d0fb9aa1a19
Alen Jo <jo.alen1@outlook.com>
2026-09-06 00:14:49 +0000

    Create LengthLastChar mapper
    Created out the LengthLastChar mapper class for part C subproblem

---
+++ src/main/java/com/joalen/DistinctCharacters/LengthLastChar.java
@@ -0,0 +1,29 @@
+package com.joalen.DistinctCharacters;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class LengthLastChar extends Mapper<Object, Text, Text, Text> {
+    private final Text outKey = new Text(); 
+    private final Text outVal = new Text(); 
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
+    { 
+        String line = value.toString().toLowerCase(); 
+        String[] tokens = line.split("[^a-z]+");
+
+        for (String token : tokens)
+        { 
+            if (!token.isEmpty())
+            { 
+                char lastChar = token.charAt(token.length() - 1);
+                outKey.set(token.length() + "," + lastChar);
+                outVal.set(token);
+
+                context.write(outKey, outVal);
+            }
+        }
+    }
+}
