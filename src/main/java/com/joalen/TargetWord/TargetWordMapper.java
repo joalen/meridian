e3299593bbd1b30bb7da2aacb41289bdf4064397
Alen Jo <jo.alen1@outlook.com>
2026-09-06 00:01:08 +0000

    Create TargetWordMapper
    Same like WordMapper BUT we only track a set of target words to which it should do key-value setting like the stage in mapper does

---
+++ src/main/java/com/joalen/TargetWord/TargetWordMapper.java
@@ -0,0 +1,36 @@
+package com.joalen.TargetWord;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class TargetWordMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private static final IntWritable ONE = new IntWritable(1);
+    private final Text word = new Text(); 
+
+    private static final Set<String> TARGETS = new HashSet<>(Arrays.asList( 
+        "ahab",
+        "captain",
+        "harpoon"
+    ));
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+    { 
+        String line = value.toString().toLowerCase(); 
+        String[] tokens = line.split("[^a-z]+");
+
+        for (String token : tokens)
+        { 
+            if (TARGETS.contains(token))
+            { 
+                word.set(token);
+                context.write(word, ONE);
+            }
+        }
+    }
+}
